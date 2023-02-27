0dd629e39f722c47cf483247e01f2491a7784e7b
mishanguyen <nguyen14@usf.edu>
2023-02-26 19:58:40 -0500

    Added size and pizza type input in the constructor

--- classOrder.h
+++ classOrder.h
@@ -1,6 +1,8 @@
 /* Class definitions for Pizza and Pasta, which are going to be used for templated queue. 
 A customer can only order either pizza or pasta. */
 /* TODO: 
+- Input for crust and toppings
+- Print order name, phone, drink, total;
 - Subclass PastaOrder and its functions
 */
 
@@ -30,6 +32,7 @@ class Order {
             cout << "Nice to meet you, " << name << "!" << endl;
             cout << "What drink would you like? Enter drink or \"none\": ";
             getline(cin, drink);
+            total = 0;
         } // default constructor
         // Order(string name, string phone, string drink, double total) // full constructor
         //     : name(name), phone(phone), drink(drink), total(total) {}
@@ -55,12 +58,75 @@ class PizzaOrder : public Order {
         string* toppings;
     public:
         PizzaOrder() : Order() {
-            cout << "Pick one of the following:" << endl;
-            cout << "1. Margherita" << endl;
-            cout << "2. Pepperoni" << endl;
-            cout << "3. Cheese" << endl;
-            cout << "4. Vegeterian" << endl;
-            cout << "5. BBQ Chicken" << endl;
+            // chooseing pizza type
+            bool valid = 0;
+            while (!valid) {
+                cout << endl;
+                cout << "Choose pizza type:" << endl;
+                cout << "1. Margherita" << endl;
+                cout << "2. Pepperoni" << endl;
+                cout << "3. Cheese" << endl;
+                cout << "4. Vegeterian" << endl;
+                cout << "5. BBQ Chicken" << endl;
+                cout << endl;
+                char pizzaChoice;
+                cout << "Enter your choice: ";
+                cin >> pizzaChoice;
+                switch (pizzaChoice) {
+                    case '1':   total += Margherita;
+                                type = Margherita;
+                                valid = 1;
+                                break;
+                    case '2':   total += Pepperoni;
+                                type = Pepperoni;
+                                valid = 1;
+                                break;
+                    case '3':   total += Cheese;
+                                type = Cheese;
+                                valid = 1;
+                                break;
+                    case '4':   total += Vegetarian;
+                                type = Vegetarian;
+                                valid = 1;
+                                break;
+                    case '5':   total += BBQChicken;
+                                type = BBQChicken;
+                                valid = 1;
+                                break;
+                    default:    cout << "Invalid choice. Please try again. " << endl;
+                                break;
+                }
+            }
+            cout << endl;
+            cout << "Wonderful choice, " << name << "!" << endl;
+            valid = 0;
+            while (!valid) {
+                cout << endl;
+                cout << "Let's choose the size:" << endl;
+                cout << "1. 14\" (+$0)" << endl;
+                cout << "2. 16\" (+$2)" << endl;
+                cout << "3. 18\" (+$4)" << endl;
+                cout << endl;
+                char sizeChoice;
+                cout << "Enter your choice: ";
+                cin >> sizeChoice;
+                switch (sizeChoice) {
+                    case '1':   size = 14;
+                                valid = 1;
+                                break;
+                    case '2':   size = 16;
+                                total += 2.0;
+                                valid = 1;
+                                break;
+                    case '3':   size = 18;
+                                total += 4.0;
+                                valid = 1;
+                                break;
+                    default:    cout << "Invalid choice. Please try again. " << endl;
+                                break;
+                }
+            }
+
         }
         PizzaType getType() const { return type; }
         int getSize() const { return size; }
